cb32074eb294fcea387172add6e10394bd024044
dmitry.abashin <dmitry.abashi@euromoneyplc.com>
2021-11-16 12:08:30 +0500

    High level design

--- PageObjects/SearchResultPage.cs
+++ PageObjects/SearchResultPage.cs
@@ -10,3 +10,17 @@ namespace JuribaKayak.SearchUIAutomation.PageObjects
         }
     }
 }
+//Design:
+
+//Wait show more results button.
+
+//Find all ticket items elements.
+
+//Transform every element to TicketItem POM.
+
+//TicketItem has: NumberOfFlights(return int), GetFlights(return Flight POM), NumberOfSeats(return int).
+
+//Flight has: direction, depart time, arrival time, company.
+
+//Do checks.
+
